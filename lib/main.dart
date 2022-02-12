0bd0f21edc43712d892f14255b6ed0c51d211abb
Shulammite <joyfuljohn96@gmail.com>
2022-02-12 08:19:17 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+    backgroundColor: Colors.black,
+    body: Center(
+      child: Image(
+        image: NetworkImage(
+            'https://images.unsplash.com/photo-1612151855475-877969f4a6cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aGQlMjBpbWFnZXxlbnwwfHwwfHw%3D&w=1000&q=80'),
+      ),
+    ),
+    appBar: AppBar(
+      centerTitle: true,
+      title: Text('I AM RICH'),
+      backgroundColor: Colors.amber,
+    ),
+  )));
+}
