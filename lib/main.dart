e3274a6d89b7f099817b775a73273e998fdce1b1
Shulammite <joyfuljohn96@gmail.com>
2022-02-12 11:23:50 +0000

    added and changed my icon and title name

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,12 @@ void main() {
     backgroundColor: Colors.black,
     body: Center(
       child: Image(
-        image: NetworkImage(
-            'https://images.unsplash.com/photo-1612151855475-877969f4a6cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aGQlMjBpbWFnZXxlbnwwfHwwfHw%3D&w=1000&q=80'),
+        image: AssetImage('images/diamond.png'),
       ),
     ),
     appBar: AppBar(
       centerTitle: true,
-      title: Text('I AM RICH YES'),
+      title: Text('I AM RICH!'),
       backgroundColor: Colors.amber,
     ),
   )));
